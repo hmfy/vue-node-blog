8a3d9e4d94d99c3db1a887ebcad9464f883325b5
unknown <paddingme@163.com>
2020-01-15 00:26:17 +0800

    'aboutMe'

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,7 +2,11 @@
     <el-card class="box-card">
         <el-divider content-position="left">关于我</el-divider>
         <div class="about-me">
-            <h1>前端小学生, 在线写Vue</h1>
+            <p @click="handleClick" class="click-edit" v-show="!isShow && isLogin"><i class="el-icon-edit"></i></p>
+            <pre class="show-about-me" v-html="currentData" v-show="!isShow"></pre>
+            <pre class="edit-about-me" v-html="currentData" ref="pre" contenteditable="true" type="textarea" v-show="isShow"/>
+            <el-button type="primary" plain @click="submitAboutMe" v-show="isShow">保存</el-button>
+            <el-button type="danger" plain @click="quitSubmit" v-show="isShow">取消</el-button>
         </div>
         <el-divider content-position="left">联系我</el-divider>
         <div class="concat-me">
@@ -13,20 +17,69 @@
 
 <script>
 
+    import {mapState} from 'vuex'
+
     export default {
         data() {
             return {
                 // loading: true
+                isShow: false,
+                currentData: ''
             }
         },
+        computed: {
+            ...mapState(['isLogin'])
+        },
         methods: {
+            handleClick () {
+                this.isShow = true;
+                this.$nextTick(()=>{
+                    console.log(this.$refs['pre'])
+                    this.$refs['pre'].click();
+                })
+            },
+            submitAboutMe () {
+                let content = this.$refs['pre'].innerHTML;
+                // 提交更改
+                this.axios({
+                    method: 'post',
+                    url: '/updateAboutMe',
+                    data: {
+                        aboutMe: content
+                    }
+                }).then(res => {
+                    this.$message.success("保存成功!");
+                    // 刷新展示面板
+                    this.currentData = content;
 
+                    this.quitSubmit();
+                }).catch(err => {
+                    console.log(err)
+                })
+            },
+            quitSubmit () {
+                this.isShow = false;
+            },
+            getAboutMe () {
+                this.axios({
+                    url: '/getAboutMe'
+                }).then(res => {
+                    if (res.data.length) {
+                        this.currentData = res.data[0].content;
+                    }
+                }).catch(err => {
+                    console.log(err)
+                })
+            }
         },
         beforeCreate() {
             const store = this.$store;
             store.commit("setLeftTagsLine", {
                 leftCol: 24
             })
+        },
+        created() {
+            this.getAboutMe();
         }
     }
 
@@ -45,5 +98,19 @@
     .about-me {
         overflow: auto;
         padding: 15px;
+    }
+
+    .about-me .show-about-me,
+    .about-me .edit-about-me{
+        height: 350px;
+        padding: 20px;
+    }
+
+    .about-me .el-button {
+        margin-top: 10px;
+    }
+
+    .about-me .click-edit {
+        padding-left: 20px;
     }
 </style>
